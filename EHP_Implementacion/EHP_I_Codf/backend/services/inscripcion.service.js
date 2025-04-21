0c85cdd1d985bf032ca491aedfce1be4bc4c6195
LucasChas <luquitaschasdiaz3@gmail.com>
2025-04-21 14:40:30 -0300

    Inscripciones (CHAS) V2.0

--- EHP_Implementacion/EHP_I_Codf/backend/services/inscripcion.service.js
+++ EHP_Implementacion/EHP_I_Codf/backend/services/inscripcion.service.js
@@ -34,27 +34,10 @@ const obtenerTodas = async () => {
     return await Inscripcion.findAll();
 };
 
-const obtenerPorDni = async (dni) => {
-    return await Inscripcion.findByPk(dni);
-};
 
-const actualizarInscripcion = async (dni, nuevosDatos) => {
-    const inscripcion = await Inscripcion.findByPk(dni);
-    if (!inscripcion) return null;
-    return await inscripcion.update(nuevosDatos);
-};
-
-const eliminarInscripcion = async (dni) => {
-    const inscripcion = await Inscripcion.findByPk(dni);
-    if (!inscripcion) return null;
-    await inscripcion.destroy();
-    return true;
-};
 
 module.exports = {
     crearInscripcion,
     obtenerTodas,
-    obtenerPorDni,
-    actualizarInscripcion,
-    eliminarInscripcion
+    
 };
--- EHP_Implementacion/EHP_I_Codf/frontend/src/api.js
+++ EHP_Implementacion/EHP_I_Codf/frontend/src/api.js
@@ -10,35 +10,8 @@ export const crearInscripcion = (datos, id) =>
   axios.post(`${API_BASE}/inscripcion/${id}`, datos); //aca deberia tener asociada la actividad como query.
   
 
-//no hace falta.
-export const obtenerInscripciones = () =>
-  axios.get(`${API_BASE}/inscripcion`);
 
-export const obtenerInscripcionPorDni = (dni) =>
-  axios.get(`${API_BASE}/inscripcion/${dni}`);
 
-export const actualizarInscripcion = (dni, datos) =>
-  axios.put(`${API_BASE}/inscripcion/${dni}`, datos);
-
-export const eliminarInscripcion = (dni) =>
-  axios.delete(`${API_BASE}/inscripcion/${dni}`);
-
-// ================== TIPO DE ACTIVIDAD ==================
-
-export const crearTipoActividad = (datos) =>
-  axios.post(`${API_BASE}/tipoActividad`, datos);
-
-export const obtenerTiposActividad = () =>
-  axios.get(`${API_BASE}/tipoActividad`);
-
-export const obtenerTipoActividadPorId = (idTipo) =>
-  axios.get(`${API_BASE}/tipoActividad/${idTipo}`);
-
-export const actualizarTipoActividad = (idTipo, datos) =>
-  axios.put(`${API_BASE}/tipoActividad/${idTipo}`, datos);
-
-export const eliminarTipoActividad = (idTipo) =>
-  axios.delete(`${API_BASE}/tipoActividad/${idTipo}`);
 
 // ================== ACTIVIDADES ==================
 
