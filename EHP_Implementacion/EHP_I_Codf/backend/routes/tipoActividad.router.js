9280bfb2a97ae2a0345599d138618f860a1b582a
Valentino <roccavalentino5@gmail.com>
2025-04-22 10:21:14 -0300

    a

--- EHP_Implementacion/EHP_I_Codf/backend/routes/tipoActividad.router.js
+++ EHP_Implementacion/EHP_I_Codf/backend/routes/tipoActividad.router.js
@@ -6,12 +6,16 @@ const router = express.Router();
 // Crear nuevo tipo de actividad
 router.post('/', async (req, res) => {
     try {
-        const nuevo = await tipoActividadService.crearTipoActividad(req.body);
-        res.status(201).json(nuevo);
+        console.log("LLEGÓ:", req.body); // <--- log importantísimo
+
+        const nueva = await inscripcionService.crearInscripcion(req.body);
+        res.status(201).json(nueva);
     } catch (error) {
+        console.error("ERROR AL CREAR INSCRIPCIÓN:", error); // 👈 esto te da la pista real
         res.status(500).json({ error: error.message });
     }
 });
+
 
 // Obtener todos los tipos de actividad
 router.get('/', async (req, res) => {
